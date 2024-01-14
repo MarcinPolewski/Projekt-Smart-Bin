eafaab5b3c6511fb8f0536425a83537f50531b2b
rmironko <01187254@pw.edu.pl>
2024-01-14 22:30:47 +0100

    poprawki od chata

--- trashAPI/TrashApp/urls.py
+++ trashAPI/TrashApp/urls.py
@@ -1,28 +1,28 @@
-from django.conf.urls import url
+from django.urls import path
 from TrashApp import views
 from TrashApp.views import *
 from rest_framework import routers
 
-api_pefix ="api/"
-
+api_prefix = "api/"
 
 router = routers.DefaultRouter()
-router.register(api_pefix + r'users', UserViewSet)
-router.register(api_pefix + r'bins', BinViewSet)
-router.register(api_pefix + r'logs', LogsViewSet)
-router.register(api_pefix + r'takeout', TakeoutViewSet)
-router.register(api_pefix + r'schedule', ScheduleViewSet)
+router.register(api_prefix + r'users', UserViewSet)
+router.register(api_prefix + r'bins', BinViewSet)
+router.register(api_prefix + r'logs', LogsViewSet)
+router.register(api_prefix + r'takeout', TakeoutViewSet)
+router.register(api_prefix + r'schedule', ScheduleViewSet)
+
 urlpatterns = [
-    url(r'^users/$', views.userApi),
-    url(r'^users/([0-9]+)', views.userApi),
-    url(r'^bins/$', views.binApi),
-    url(r'^bins/([0-9]+)', views.binApi),
-    url(r'^logs/$', views.logsApi),
-    url(r'^logs/([0-100]+)', views.logsApi),
-    url(r'^schedule/$', views.scheduleApi),
-    url(r'^schedule/([0-100]+)', views.scheduleApi),
-    url(f'^takeout/$', views.takeoutApi),
-    url(r'^takeout/([0-9]+)', views.takeoutApi)
+    path('users/', views.userApi),
+    path('users/<int:pk>/', views.userApi),
+    path('bins/', views.binApi),
+    path('bins/<int:pk>/', views.binApi),
+    path('logs/', views.logsApi),
+    path('logs/<int:pk>/', views.logsApi),
+    path('schedule/', views.scheduleApi),
+    path('schedule/<int:pk>/', views.scheduleApi),
+    path('takeout/', views.takeoutApi),
+    path('takeout/<int:pk>/', views.takeoutApi),
 ]
 
 urlpatterns += router.urls
--- trashAPI/trashAPI/urls.py
+++ trashAPI/trashAPI/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url, include
+from django.urls import path, re_path  # Dodaj re_path do importów
+from django.conf.urls import include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r"^", include('TrashApp.urls'))
+    re_path(r"^", include('TrashApp.urls'))  # Zastąp url() przez re_path()
 ]
