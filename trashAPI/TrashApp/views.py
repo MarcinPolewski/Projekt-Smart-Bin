9ef2b97764a05829d7752d8489eb20475d110723
Elbergg <61651967+Elbergg@users.noreply.github.com>
2024-01-17 19:44:08 +0100

    dodalem kompatybilnosc takeout z reuqests.post

--- trashAPI/TrashApp/views.py
+++ trashAPI/TrashApp/views.py
@@ -7,6 +7,8 @@ from TrashApp.serializers import *
 from rest_framework.viewsets import ModelViewSet
 from django.forms.models import model_to_dict
 import json
+import urllib.parse
+
 # Create your views here.
 
 
@@ -128,26 +130,34 @@ def takeoutApi(request, id=0):
         takeout_serializer = TblWynoszenieSerializer(takeout, many=True)
         return JsonResponse(takeout_serializer.data,safe=False)
     elif request.method == "POST":
-        takeout_data=JSONParser().parse(request)
+        print(request)
+        print(request.body)
+        try:
+            takeout_data=JSONParser().parse(request)
+        except Exception:
+            reuquest_without_b = str(request.body).strip("b'")
+            request_decoded = urllib.parse.parse_qs(reuquest_without_b)
+            takeout_data=request_decoded
+            for field in takeout_data:
+                print(field)
+                takeout_data.update({field: takeout_data.get(field)[0]})
         takeout_serializer = TblWynoszenieSerializer(data=takeout_data)
-        print(takeout_data)
-        add_points = takeout_data.get('add_points')
+        add_points = int(takeout_data.get('add_points'))
         who_did = TblUzytkownicyKonfig.objects.get(id_user=takeout_data['who_should'])
         user_serializer = TblUzytkownicyKonfigSerializer(who_did)
         user_data=user_serializer.data
-        print(user_data)
         current_user_points = user_data['points_status']
-        print(current_user_points)
         current_user_points += add_points
         user_data.update({"points_status": current_user_points})
-        print(user_data)
         user_serializer_new = TblUzytkownicyKonfigSerializer(who_did, data = user_data)
+        print(takeout_data)
+
         takeout_serializer = TblWynoszenieSerializer(data=takeout_data)
         print(takeout_data)
-        print(takeout_serializer.data)
         if takeout_serializer.is_valid():
             takeout_serializer.save()
-        print(takeout_serializer.data)
+            print(takeout_serializer.data)
+            print("haaalo")
         if user_serializer_new.is_valid():
             user_serializer_new.save()
             return JsonResponse("Added succesfully",safe=False)
